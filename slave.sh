e15b1061b76d2f65669ce1b95033fb25a1a25129
Ishaq Suleiman <68830069+nitromove@users.noreply.github.com>
2023-10-23 01:39:27 +1400

    Create slave.sh

---
+++ slave.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Update the application packages
+
+# Clone the Laravel repository from GitHub
+sudo apt install git -y
+cd /var/www/
+sudo git clone https://github.com/laravel/laravel.git
+
+# Change ownership of the Laravel directory
+sudo chown -R www-data:www-data /var/www/laravel
+
+# Installing and enabling apache
+sudo apt install apache2 -y
+sudo ufw allow in "Apache"
+sudo ufw status
+
+#Installing mySQl
+sudo apt install mysql-server -y
+sudo mysql
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+exit
+sudo mysql_secure_installation -y
+
+Installing PHP and its required modules
+sudo apt install php libapache2-mod-php php-mysql -y
+
+Verifying PHP version
+php -v
+
+Setting up Laravel virtual host
+laravel_conf_file="/etc/apache2/sites-available/laravel.conf"
+cat > "$laravel_conf_file" <<EOF
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/laravel/public
+    ServerName localhost
+
+    <Directory /var/www/laravel/public>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+# Enabling the Laravel site and disable the default site
+a2ensite laravel
+a2dissite 000-default
+
+# Reloading Apache to apply changes
+systemctl reload apache2
+
+# Creating a PHP info file
+php_info_file="/var/www/default/info.php"
+echo "<?php phpinfo();" > "$php_info_file"
+
+# Copy php file to server directory
+sudo cp test.php /var/www/html/
+
+
+# Enabling the PHP info site
+a2ensite default
+
+# Reload Apache to apply changes
+systemctl reload apache2
